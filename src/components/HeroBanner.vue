62903da3bb00e310ef2285496fee4d4e98b497e8
gayathris99 <gayathri11499@gmail.com>
2024-01-16 21:07:15 +0530

    First Commit

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    hero banner
+  </div>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <hero-banner/>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import HeroBanner from 'src/components/HeroBanner.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { HeroBanner }
+})
+</script>

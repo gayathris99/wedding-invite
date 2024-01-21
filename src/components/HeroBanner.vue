618647e353849ad908ba76bfbc0512c1c37b710a
gayathris99 <gayathri11499@gmail.com>
2024-01-21 09:15:02 +0530

    Hero Banner

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,5 +1,93 @@
 <template>
   <div>
-    hero banner
-  </div>
+    <div class="parallax">
+      <div class="parallax-content q-pa-xl column items-center justify-center align-center">
+        <div class="font-josefin fw-300 text-center">
+           G A Y A T H R I &nbsp; & &nbsp;  <br v-if="isMobile"/>S H A S H A N K
+        </div>
+        <div class="font-spectral style-italic q-mt-sm fs-20 fw-300">
+          Are getting married
+        </div>
+      </div>
+    </div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus beatae, qui nulla consequuntur aut provident asperiores temporibus dolore aperiam non vero omnis, tenetur ad inventore dolores, nam cum assumenda dicta?
+    Vitae optio est, labore dolores culpa corporis iure cupiditate veritatis sunt enim amet, praesentium incidunt. Atque incidunt exercitationem, deleniti accusantium, sapiente dolorum saepe optio accusamus molestias repudiandae consequuntur placeat harum.
+    Quam aspernatur velit impedit architecto sit error nam repudiandae? Expedita, harum ipsam et fuga quibusdam porro veritatis deleniti, nihil nemo ipsa hic minima delectus. Accusamus doloremque repellendus cum temporibus maiores?
+    Sapiente porro alias voluptatum voluptate tenetur assumenda eligendi officia harum unde nesciunt minus, voluptatem repudiandae eaque fuga doloribus dolore ipsum. Incidunt temporibus impedit esse repellendus eaque eligendi dolor facilis fugiat!
+    Voluptatem blanditiis cupiditate quam quaerat labore autem repellendus dicta eaque laboriosam, placeat esse eos architecto, laborum aut odio nostrum rem sit? Iure, earum cum hic voluptas recusandae voluptates architecto inventore!
+    Nostrum voluptatibus modi vel, debitis repellendus harum obcaecati quo et distinctio necessitatibus rerum, aspernatur accusamus, est possimus soluta aut numquam facilis repudiandae quos? Cupiditate, delectus numquam voluptas inventore quos aliquam.
+    Adipisci quis eius aperiam quod expedita repellat quibusdam, sit nihil alias. In dolores inventore labore magnam voluptas cumque nobis accusamus sequi at tempore non eaque necessitatibus, voluptates saepe autem doloribus!
+    Molestias quas quaerat quia! Excepturi fugiat quasi placeat aspernatur enim voluptatem inventore fugit, voluptatibus ipsum doloribus quisquam quas dignissimos eaque dolorem voluptates maiores. Quos magnam eos totam velit vero voluptates.
+    Explicabo, doloribus reiciendis harum at, vitae quo fugit minus eveniet, animi ipsum tenetur. Voluptas, eligendi? Ratione, minus non consequatur totam fugit sed quam eligendi sapiente accusamus quos eius quas iure.
+    Maiores provident dolores necessitatibus consequatur reprehenderit esse error voluptatem quod, obcaecati voluptas facilis debitis dicta incidunt nulla at aspernatur? Dignissimos facere inventore debitis quidem nihil necessitatibus modi sunt qui exercitationem!
+    Voluptatibus minima quidem ipsa pariatur alias ipsam odit adipisci deleniti earum, libero ducimus reiciendis ex deserunt recusandae ea cupiditate? Cupiditate, hic! Natus reprehenderit aperiam veritatis facilis maxime quibusdam cumque earum.
+    Ex nobis est eveniet, nulla deleniti magni enim autem perspiciatis odio vitae repellat rem obcaecati ducimus modi dicta. Alias provident error enim accusantium quaerat dolore, voluptate ad dignissimos voluptatem blanditiis.
+    Nisi, error velit possimus quia laborum dolor molestiae? Iure illo consectetur tempora harum sunt alias perspiciatis sed incidunt ipsum sequi, blanditiis, in nisi! Voluptatem quam quidem animi, beatae vel voluptatum!
+    Quae optio, eos, neque labore nesciunt aut magnam nam deserunt fugit perferendis deleniti dolore et quam eius ipsa maiores saepe blanditiis voluptate cum aliquam aspernatur? Dolorum voluptatem pariatur consectetur voluptates.
+    Vel nemo autem, laboriosam laborum ea quam labore officiis ab vero provident placeat eos iusto minima impedit molestiae. Quia maxime vel qui, cumque voluptate dolorem consequatur esse reprehenderit modi in!
+    Odio veritatis laboriosam id non reiciendis corporis fugiat aliquid sunt iure distinctio minus obcaecati ipsam enim, eum at quia, excepturi voluptate aut et voluptatem dicta ex. Expedita omnis pariatur amet.
+    Quibusdam asperiores temporibus, perferendis saepe quam neque incidunt doloribus natus? Facilis minima delectus, earum praesentium illo at veritatis, iste atque, odio aliquid a maxime perferendis pariatur exercitationem sed alias dolore.
+    Earum, velit? Ut possimus rem, dicta voluptate sequi aliquid hic cum illum quae laudantium esse architecto ex, debitis molestiae dolore repudiandae delectus facere vero perspiciatis exercitationem accusamus nam quia voluptates?
+    Culpa vero nesciunt sapiente voluptate alias. Maiores impedit repudiandae, corrupti nesciunt, voluptatem reiciendis, voluptas dolor laudantium quisquam quasi deleniti sint dicta reprehenderit obcaecati. Numquam mollitia quisquam tenetur, beatae consequuntur veniam?
+    Provident necessitatibus veniam impedit unde dolorum corporis nulla excepturi quos veritatis voluptate fuga, recusandae maxime facilis nobis optio incidunt iure cupiditate natus vel dicta alias autem. Libero commodi nobis molestiae.
+    Nemo nobis quidem culpa quisquam itaque possimus quod velit in commodi repudiandae quo praesentium ipsam explicabo minus voluptates numquam architecto beatae facere quam quia, aut illo facilis deleniti fuga? Error?
+    Rerum iure amet, quia incidunt dolor fugiat, nam libero dolorem sed eius cum possimus? Libero voluptatibus corporis quidem voluptatum, omnis natus quas rerum voluptate cupiditate. Non eos ad voluptate in.
+    Possimus non quae consequuntur modi odio tenetur quos aperiam, ex voluptas fuga, commodi illum tempora eos ad nisi fugiat necessitatibus. Fugiat illo error odit iusto necessitatibus facilis earum corrupti cupiditate.
+    Odit beatae veritatis necessitatibus aperiam hic voluptatem placeat est quas sint ad debitis totam delectus, maxime blanditiis vel molestias corrupti perferendis atque corporis repellat nesciunt? Porro illum dicta minus quo.
+    Ab iste incidunt autem consectetur nostrum sed necessitatibus quam eligendi nulla dolor voluptatum saepe, qui aspernatur sapiente minima tenetur commodi laudantium. Eos quibusdam iure est minima mollitia iusto odit modi.
+    Enim ipsam veritatis soluta culpa ea perferendis sed. Maxime odit eveniet maiores minima exercitationem. Alias neque, suscipit, debitis repellat consectetur quis magni aliquid doloribus odit dolorum quidem maxime facilis eum.
+    Consequatur aperiam animi est possimus, at reprehenderit earum quos officiis hic mollitia tempore, optio dolorem iure quas aut, delectus nulla aliquid. Similique, aliquam? Atque repellendus, dolorum repudiandae excepturi suscipit quas.
+    Assumenda nisi aliquid qui aliquam fuga suscipit, perspiciatis tempora consequatur magni dignissimos mollitia voluptatem odit libero reprehenderit rem? Quia, saepe? Aut vero accusamus, quas optio asperiores est sit qui voluptate.
+    Culpa, omnis dolor! Doloremque, tempore iusto? Possimus perspiciatis rem similique reiciendis ducimus ut, ratione placeat commodi illo doloremque quam itaque velit corrupti voluptatem suscipit animi adipisci consequuntur eaque molestias libero.
+    Aut dolore culpa assumenda praesentium magnam odit, quia excepturi placeat cumque hic repudiandae, quisquam, esse quidem. Fugit aspernatur mollitia quasi sapiente odit neque quis id, magni vel harum voluptate modi!</div>
+   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HeroBanner',
+  data () {
+    return {
+      isMobile: this.$q.screen.lt.sm
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.parallax {
+  /* The image used */
+  // position: relative;
+  background-image: url("~assets/RosesImage.jpg");
+  // opacity: 0.6;
+
+  /* Set a specific height */
+  min-height: 750px;
+  // height: 1000px;
+  // max-height: 1000px;
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+    @media only screen and (max-width: $breakpoint-xs-max) {
+      min-height: 400px;
+  }
+}
+.parallax-content {
+  // position: absolute;
+  // margin: auto;
+  // width: 50%;
+}
+.font-josefin {
+  font-size: 32px;
+  margin-top: 48px;
+    @media only screen and (max-width: $breakpoint-xs-max) {
+      font-size: 20px;
+      margin-top: 0px;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <hero-banner/>
   </q-page>
 </template>
